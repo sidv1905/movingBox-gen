eae5965aed96330c5e1d9e55cafc1fa02fe16116
sidv1905 <sidvarangaonkar1905@gmail.com>
2022-03-02 13:09:29 +0530

    component setup, default css

---
+++ src/components/MovingBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from '../styles/box.module.scss'
+export default function MovingBox() {
+  return (
+    <div className={styles.movingBox}>MovingBox</div>
+  )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,47 +1,15 @@
 import { useState } from "react";
 
 import "../styles/App.css";
-
+import MovingBox from "../components/MovingBox";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        {count > 0 ? (
-          <p>
-            <code>The count is now: {count}</code>
-          </p>
-        ) : null}
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <h1>Moving Box App</h1>
+      <MovingBox/>
+      <button onClick={() => setCount(count + 1)}>Add</button>
     </div>
   );
 }
