e10f972b2ff2c5145f5244a10cebf3062765e95d
sidv1905 <sidvarangaonkar1905@gmail.com>
2022-03-03 07:48:46 +0530

    added event handlers and css for dragging

--- src/components/MovingBox.tsx
+++ src/components/MovingBox.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
-import styles from '../styles/box.module.scss'
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../styles/box.module.scss";
 export default function MovingBox() {
+  const [pressed, setPressed] = useState(false);
+  const boxRef = useRef<HTMLDivElement>(null);
+  function handleMouseDown() {
+    console.log("mouse down");
+    setPressed(true);
+  }
+
+  function handleMouseMove(e: any) {
+    console.log("Mouse move");
+  }
+  function handleMouseUp() {
+    console.log("mouse up");
+    setPressed(false);
+  }
+  useEffect(() => {
+    if (pressed) {
+      console.log("moving");
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    }
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [pressed]);
   return (
-    <div className={styles.movingBox}>MovingBox</div>
-  )
+    <div
+      ref={boxRef}
+      onMouseDown={handleMouseDown}
+      className={styles.movingBox}
+    >
+      Moving box
+    </div>
+  );
 }
